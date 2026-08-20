4b3d46a8f1dd7b08cc0aac63b0b74b191d5706d7
Sobhan Eyvazipoor <Sobhansbnvzpr817143@gmail.com>
2026-08-20 04:54:13 +0430

    integrated blog content with templates
    added author, category, and image fields to the blog model, merged the blog tabs, connected blog views to their models, and created reusable blog writer and ads partials with template includes

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     #empty_value_display = '-empty-'
     list_display = ('title', 'counted_views', 'status', 'published_date')
-    list_filter = ('status',)
+    list_filter = ('status', 'author')
     search_fields = ['title', 'content']
     #ordering = ['-created_date']
     
-        
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 # Create your views here.
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context=context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    posts = Post.objects.filter(status=1)
+    post = get_object_or_404(posts, pk=pid)
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context=context)
